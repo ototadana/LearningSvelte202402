76ffb651373399202652801914410495b97e6cfc
ototadana <ototadana@gmail.com>
2024-02-25 20:09:01 +0900

    fix Month.svelte

--- src/lib/Month.svelte
+++ src/lib/Month.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	const today = new Date()
-	let months = [1, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+	let months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 	export let month = today.getMonth() + 1
 	export let fieldName = 'month'
 </script>
